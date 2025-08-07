bff07fa19df2c650165c51cb20fb321d5b345894
SemihErgili <ergilisemih7@gmail.com>
2025-08-08 02:15:00 +0300

    sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,40 +1,42 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const lastModified = new Date().toISOString()
+
   return [
     {
       url: 'https://dinletiyo.com',
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: 'https://dinletiyo.com/login',
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: 'https://dinletiyo.com/signup',
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: 'https://dinletiyo.com/home',
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'daily',
       priority: 0.9,
     },
     {
       url: 'https://dinletiyo.com/gizlilik',
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'yearly',
       priority: 0.3,
     },
     {
       url: 'https://dinletiyo.com/kosullar',
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'yearly',
       priority: 0.3,
     },
